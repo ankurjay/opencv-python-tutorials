c03cd550f2ee86c9fe23cd5c4db51c151fb84747
ankurjay <ankur.jay007@gmail.com>
2025-11-15 19:44:20 -0500

    Add 00.01 script to load, display and save images in GUI

---
+++ 00 GUI Features/01_load_display_and_save_images.py
@@ -0,0 +1,30 @@
+import sys
+from tutorial_utils import utils
+import cv2
+
+# File path to image used in this tutorial
+DATA_DIR = utils.data_dir()
+
+
+# Read the image
+img = cv2.imread(DATA_DIR + 'starry_night.jpg')
+
+# Check if the image was loaded correctly
+if img is None:
+    sys.exit("Could not read the image!")
+    
+print("Image loaded. Displaying...")
+
+# Create a Window
+cv2.namedWindow('Starry Night', cv2.WINDOW_AUTOSIZE)
+
+# Display image in the window
+cv2.imshow("Starry Night", img)
+
+# Wait for a key press to close the image
+k = cv2.waitKey(0)
+
+# If the key pressed was 's', save the iamge
+if k == ord('s'):
+    cv2.imwrite("/tmp/starry_night_saved.png", img)
+
